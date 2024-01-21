ed7398d891052f0f5c0390914029ca3fa2d1763e
Mzdyl <25149539+Mzdyl@users.noreply.github.com>
2024-01-21 20:32:06 +0800

    mac 脚本新版适配与优化

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 
 mac/linux 终端运行对应脚本即可
 
-windows 下使用python 运行对应脚本即可
+windows 下使用 python 运行对应脚本即可
 
 ***目前只针对全新安装做了适配，升级安装等情况尚不完善***
 
--- install_mac.sh
+++ install_mac.sh
@@ -3,17 +3,64 @@ sudo -v
 
 echo "正在拉取最新版本的仓库..."
 cd /tmp
+rm -rf LiteLoader
 git clone https://github.com/LiteLoaderQQNT/LiteLoaderQQNT.git LiteLoader
 
 # 移动到安装目录
 echo "拉取完成，正在安装LiteLoader..."
-sudo cp LiteLoader/src/preload.js /Applications/QQ.app/Contents/Resources/app/application/preload.js
-mv LiteLoader $HOME/Library/Containers/com.tencent.qq/Data/Documents/
+sudo cp -f LiteLoader/src/preload.js /Applications/QQ.app/Contents/Resources/app/application/preload.js
+
+# 如果目标目录存在且不为空，则先备份处理
+if [ -e "$HOME/Library/Containers/com.tencent.qq/Data/Documents/LiteLoader" ]; then
+    # 删除上次的备份
+    rm -rf "$HOME/Library/Containers/com.tencent.qq/Data/Documents/LiteLoader_bak"
+
+    # 将已存在的目录重命名为LiteLoader_bak
+    mv "$HOME/Library/Containers/com.tencent.qq/Data/Documents/LiteLoader" "$HOME/Library/Containers/com.tencent.qq/Data/Documents/LiteLoader_bak"
+    echo "已将原LiteLoader目录备份为LiteLoader_bak"
+fi
+
+
+# 移动LiteLoader
+mv -f LiteLoader $HOME/Library/Containers/com.tencent.qq/Data/Documents/
+
+# 如果LiteLoader_bak中存在data文件夹，则复制到新的LiteLoader目录
+if [ -d "$HOME/Library/Containers/com.tencent.qq/Data/Documents/LiteLoader_bak/data" ]; then
+    cp -r "$HOME/Library/Containers/com.tencent.qq/Data/Documents/LiteLoader_bak/data" "$HOME/Library/Containers/com.tencent.qq/Data/Documents/LiteLoader/"
+    echo "已将 LiteLoader_bak 中的 data 文件夹复制到新的 LiteLoader 目录"
+fi
 
 # 自定义插件，默认为~/Downloads/LiteLoaderPlugins
-read -p "请输入LiteLoader插件目录（默认为$HOME/Downloads/plugins）: " custompluginsDir
-pluginsDir=${custompluginsDir:-"$HOME/Downloads"}
+read -p "请输入LiteLoader插件目录（默认为$HOME/Downloads/Plugins）: " custompluginsDir
+pluginsDir=${custompluginsDir:-"$HOME/Downloads/Plugins"}
 echo "插件目录: $pluginsDir"
+
+## 设置通过环境变量插件目录
+#if [ -n "$ZDOTDIR" ]; then
+#    config_dir="$ZDOTDIR"
+#else
+#    config_dir="$HOME"
+#fi
+
+#临时方案，待完善
+config_dir="$HOME"
+
+# 检查是否已存在LITELOADERQQNT_PROFILE
+if grep -q "export LITELOADERQQNT_PROFILE=" "$config_dir/.zshrc"; then
+    read -p "LITELOADERQQNT_PROFILE 已存在，是否要修改？ (y/n): " modify_choice
+    if [ "$modify_choice" = "y" ] || [ "$modify_choice" = "Y" ]; then
+        # 如果用户同意修改，则替换原有的行
+        sed -i '' 's|export LITELOADERQQNT_PROFILE=.*|export LITELOADERQQNT_PROFILE="'$pluginsDir'"|' "$config_dir/.zshrc"
+        echo "LITELOADERQQNT_PROFILE 已修改为: $pluginsDir"
+    else
+        echo "未修改 LITELOADERQQNT_PROFILE。"
+    fi
+else
+    # 如果不存在，则添加新的行
+    echo 'export LITELOADERQQNT_PROFILE="'$pluginsDir'"' >> "$config_dir/.zshrc"
+    echo "已添加 LITELOADERQQNT_PROFILE: $pluginsDir"
+fi
+
 
 # 创建pluginsDir目录
 if [ ! -d "$pluginsDir" ]; then
@@ -26,20 +73,20 @@ cd /Applications/QQ.app/Contents/Resources/app/app_launcher
 
 # 修改index.json
 echo "正在修补index.json..."
-if ! grep -q "require('/Users" index.js; then
-    sudo sed -i '' "1i\\
+
+# 检查是否已存在相同的修改
+if grep -q "require('$HOME/Library/Containers/com.tencent.qq/Data/Documents/LiteLoader');" index.js; then
+    echo "index.json 已包含相同的修改，无需再次修改。"
+else
+    # 如果不存在，则进行修改
+    if ! grep -q "require('/Users" index.js; then
+        sudo sed -i '' "1i\\
 require('$HOME/Library/Containers/com.tencent.qq/Data/Documents/LiteLoader');\
 " index.js
+        echo "已修补 index.json。"
+    fi
 fi
 
-# 创建软连接
-#mkdir $HOME/Library/Containers/com.tencent.qq/Data/Documents/LiteLoader/plugins
-open /Applications/QQ.app #通过程序自己创建 plugins 目录，否则会无法读取插件（血的教训
-liteLoaderPluginsDir="$HOME/Library/Containers/com.tencent.qq/Data/Documents/LiteLoader/plugins"
-sudo ln -s "$liteLoaderPluginsDir" "$pluginsDir"
-
-# 提示消息
-echo "软链接已创建，从 $liteLoaderPluginsDir 到 $pluginsDir"
 
 echo "安装完成！脚本将在3秒后退出..."
 
