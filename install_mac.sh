092cccf1808c9d3e9e669a6b972087a1d018b1e2
Mzdyl <25149539+Mzdyl@users.noreply.github.com>
2024-01-21 21:02:23 +0800

    mac 脚本会退回软连接方案(新版macOS上GUI 环境变量无法永久设置)

--- install_mac.sh
+++ install_mac.sh
@@ -24,48 +24,35 @@ fi
 # 移动LiteLoader
 mv -f LiteLoader $HOME/Library/Containers/com.tencent.qq/Data/Documents/
 
-# 如果LiteLoader_bak中存在data文件夹，则复制到新的LiteLoader目录
+# 如果LiteLoader_bak中存在plugins和data文件夹，则复制到新的LiteLoader目录
 if [ -d "$HOME/Library/Containers/com.tencent.qq/Data/Documents/LiteLoader_bak/data" ]; then
+    cp -r "$HOME/Library/Containers/com.tencent.qq/Data/Documents/LiteLoader_bak/plugins" "$HOME/Library/Containers/com.tencent.qq/Data/Documents/LiteLoader/"
     cp -r "$HOME/Library/Containers/com.tencent.qq/Data/Documents/LiteLoader_bak/data" "$HOME/Library/Containers/com.tencent.qq/Data/Documents/LiteLoader/"
-    echo "已将 LiteLoader_bak 中的 data 文件夹复制到新的 LiteLoader 目录"
+    echo "已将 LiteLoader_bak 中旧数据复制到新的 LiteLoader 目录"
 fi
 
-# 自定义插件，默认为~/Downloads/LiteLoaderPlugins
-read -p "请输入LiteLoader插件目录（默认为$HOME/Downloads/Plugins）: " custompluginsDir
-pluginsDir=${custompluginsDir:-"$HOME/Downloads/Plugins"}
-echo "插件目录: $pluginsDir"
+# 是否为插件目录创建软连接
+read -p "是否为插件目录创建软连接方便安装插件 (y/N): " create_symlink
+if [ "$create_symlink" = "y" ] || [ "$create_symlink" = "Y" ]; then
+    # 自定义插件，默认为~/Downloads/plugins
+    read -p "请输入LiteLoader插件目录（默认为$HOME/Downloads/plugins）: " custompluginsDir
+    pluginsDir=${custompluginsDir:-"$HOME/Downloads"}
+    echo "插件目录: $pluginsDir"
 
-## 设置通过环境变量插件目录
-#if [ -n "$ZDOTDIR" ]; then
-#    config_dir="$ZDOTDIR"
-#else
-#    config_dir="$HOME"
-#fi
-
-#临时方案，待完善
-config_dir="$HOME"
-
-# 检查是否已存在LITELOADERQQNT_PROFILE
-if grep -q "export LITELOADERQQNT_PROFILE=" "$config_dir/.zshrc"; then
-    read -p "LITELOADERQQNT_PROFILE 已存在，是否要修改？ (y/n): " modify_choice
-    if [ "$modify_choice" = "y" ] || [ "$modify_choice" = "Y" ]; then
-        # 如果用户同意修改，则替换原有的行
-        sed -i '' 's|export LITELOADERQQNT_PROFILE=.*|export LITELOADERQQNT_PROFILE="'$pluginsDir'"|' "$config_dir/.zshrc"
-        echo "LITELOADERQQNT_PROFILE 已修改为: $pluginsDir"
-    else
-        echo "未修改 LITELOADERQQNT_PROFILE。"
+    # 创建pluginsDir目录
+    if [ ! -d "$pluginsDir" ]; then
+        mkdir -p "$pluginsDir"
+        echo "已创建插件目录: $pluginsDir"
     fi
-else
-    # 如果不存在，则添加新的行
-    echo 'export LITELOADERQQNT_PROFILE="'$pluginsDir'"' >> "$config_dir/.zshrc"
-    echo "已添加 LITELOADERQQNT_PROFILE: $pluginsDir"
-fi
 
+    # 创建软连接
+    liteLoaderPluginsDir="$HOME/Library/Containers/com.tencent.qq/Data/Documents/LiteLoader/plugins"
+    if [ ! -d "$liteLoaderPluginsDir" ]; then
+        mkdir -p "$liteLoaderPluginsDir"
+    fi
 
-# 创建pluginsDir目录
-if [ ! -d "$pluginsDir" ]; then
-    mkdir -p "$pluginsDir"
-    echo "已创建插件目录: $pluginsDir"
+    sudo ln -s "$liteLoaderPluginsDir" "$pluginsDir"
+    echo "已为插件目录创建软连接到 $pluginsDir"
 fi
 
 # 进入安装目录
@@ -86,7 +73,6 @@ require('$HOME/Library/Containers/com.tencent.qq/Data/Documents/LiteLoader');\
         echo "已修补 index.json。"
     fi
 fi
-
 
 echo "安装完成！脚本将在3秒后退出..."
 
