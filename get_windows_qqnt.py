30119b626a2ebf927befd7c5d5431b731e09da76
LY-Xiang <3047563007@qq.com>
2024-05-03 09:42:26 +0800

    链接获取逻辑优化
    防止请求到的js发生格式变更引发问题
    
    正在计划全版本qqnt自动化

--- get_windows_qqnt.py
+++ get_windows_qqnt.py
@@ -1,5 +1,6 @@
 from requests import get
 r=get('https://cdn-go.cn/qq-web/im.qq.com_new/latest/rainbow/windowsDownloadUrl.js').text
-r=r[r.find('ntDownloadX64Url')+19:]
+r=r[r.find('"ntDownloadX64Url"'):]
+r=r[r.find('"'):]
 with open('QQInstaller.exe','wb') as QQ:
-	QQ.write(get(r[:r.find('"')],stream=True).content)
+	QQ.write(get(r[1:r.find('"')],stream=True).content)
