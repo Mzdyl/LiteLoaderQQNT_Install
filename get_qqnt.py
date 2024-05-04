36629980bc8a4d3ed404520c94ed5e2fafd7feee
LY-Xiang <3047563007@qq.com>
2024-05-04 19:55:26 +0800

    格式化&信息显示
    格式:Black

--- get_qqnt.py
+++ get_qqnt.py
@@ -1,20 +1,24 @@
 from requests import get
-from sys import exit,argv
-plat=argv[1]
-if plat=='Windows':
-    url='https://cdn-go.cn/qq-web/im.qq.com_new/latest/rainbow/windowsDownloadUrl.js'
-    key='ntDownloadX64Url'
-    name='QQInstaller.exe'
-elif plat=='Linux':
+from sys import exit, argv
+
+plat = argv[1]
+print("plat:%s" % plat)
+if plat == "Windows":
+    url = "https://cdn-go.cn/qq-web/im.qq.com_new/latest/rainbow/windowsDownloadUrl.js"
+    key = "ntDownloadX64Url"
+    name = "QQInstaller.exe"
+elif plat == "Linux":
     pass
-elif plat=='Macos':
+elif plat == "Macos":
     pass
 else:
     exit(-1)
-r=get(url).text
-r=r[r.find(key):]
-r=r[r.find('"')+1:]
-r=r[r.find('"')+1:]
-with open(name,'wb') as QQ:
-    for chunk in get(r[:r.find('"')],stream=True).iter_content(chunk_size=4096):
+r = get(url).text
+r = r[r.find(key) :]
+r = r[r.find('"') + 1 :]
+r = r[r.find('"') + 1 :]
+r = r[: r.find('"')]
+print("url:%s" % r)
+with open(name, "wb") as QQ:
+    for chunk in get(r, stream=True).iter_content(chunk_size=4096):
         QQ.write(chunk)
