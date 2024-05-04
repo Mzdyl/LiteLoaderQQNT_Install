1f558081d5b40cbe641b823439bb87f1f5ff779b
LY-Xiang <3047563007@qq.com>
2024-05-04 19:04:13 +0800

    优化

--- get_windows_qqnt.py
+++ get_windows_qqnt.py
@@ -1,6 +1,8 @@
 from requests import get
 r=get('https://cdn-go.cn/qq-web/im.qq.com_new/latest/rainbow/windowsDownloadUrl.js').text
-r=r[r.find('"ntDownloadX64Url"'):]
-r=r[r.find('"'):]
+r=r[r.find('ntDownloadX64Url'):]
+r=r[r.find('"')+1:]
+r=r[r.find('"')+1:]
 with open('QQInstaller.exe','wb') as QQ:
-	QQ.write(get(r[1:r.find('"')],stream=True).content)
+    for chunk in get(r[:r.find('"')],stream=True).iter_content(chunk_size=4096):
+        QQ.write(chunk)
