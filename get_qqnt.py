fc42e268e66c84488688383dc00cb100a9d285df
LY-Xiang <3047563007@qq.com>
2024-05-04 20:40:47 +0800

    qqnt链接自动获取 (#48)
    * 链接获取逻辑优化
    
    防止请求到的js发生格式变更引发问题
    
    正在计划全版本qqnt自动化
    
    * Create get_qqnt.py
    
    * move
    
    * 优化
    
    * 优化
    
    * 更改平台获取方式
    
    目前为传参
    计划利用py原生函数识别平台
    
    * 更新 windows.yml
    
    * 格式化&信息显示
    
    格式:Black
    
    * Linux deb
    
    * 更新 linux.yml
    
    * 更新 windows.yml
    
    * 更新 get_qqnt.py
    
    * fix
    
    少打个 -
    ......
    
    * 更新 linux.yml
    
    * 更新 get_qqnt.py

---
+++ get_qqnt.py
@@ -0,0 +1,31 @@
+from requests import get
+from sys import exit, argv
+
+plat = argv[1]
+print("plat:%s" % plat)
+if plat == "Windowsx64":
+    r = get(
+        "https://cdn-go.cn/qq-web/im.qq.com_new/latest/rainbow/windowsDownloadUrl.js"
+    ).text
+    key = "ntDownloadX64Url"
+    name = "QQInstaller.exe"
+elif plat == "Debianx64":
+    r = get(
+        "https://cdn-go.cn/qq-web/im.qq.com_new/latest/rainbow/linuxQQDownload.js"
+    ).text
+    r = r[r.find("x64DownloadUrl") :]
+    r = r[: r.find("}")]
+    key = "deb"
+    name = "LinuxQQ.deb"
+elif plat == "Macos":
+    pass
+else:
+    exit(-1)
+r = r[r.find(key) :]
+r = r[r.find('"') + 1 :]
+r = r[r.find('"') + 1 :]
+r = r[: r.find('"')]
+print("url:%s" % r)
+with open(name, "wb") as QQ:
+    for chunk in get(r, stream=True).iter_content(chunk_size=4096):
+        QQ.write(chunk)
