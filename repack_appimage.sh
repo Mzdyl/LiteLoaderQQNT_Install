a4b31bc4a7750cda1443486f26569725af43a534
jbwfu <75001777+jbwfu@users.noreply.github.com>
2024-12-09 14:52:34 +0800

    feat: 手动提取 squashfs 内容以支持 arm64 架构
    - 使用 wget 替换 curl
    - 通过特征值 "68 73 71 73" 获取 squashfs 在 appimage 文件中的偏移值以提取 squashfs 文件

--- repack_appimage.sh
+++ repack_appimage.sh
@@ -5,25 +5,25 @@ os_arch=${ARCH:=$(uname -m)}
 [ "$os_arch" = "x86_64" ] && _arch="$os_arch" || _arch="aarch64"
 runtime_url="https://github.com/AppImage/AppImageKit/releases/download/13/runtime-$_arch"
 
+echo "正在获取 LiteLoaderQQNT 版本..."
 liteloaderqqnt_check_url="https://github.com/LiteLoaderQQNT/LiteLoaderQQNT/releases/latest"
-liteloaderqqnt_version=$(basename "$(wget --spider "$liteloaderqqnt_check_url" 2>&1 | grep -m1 -o 'https://.*releases/tag[^ ]*')")
+liteloaderqqnt_version=$(basename "$(wget -t3 -T3 --spider "$liteloaderqqnt_check_url" 2>&1 | grep -m1 -o 'https://.*releases/tag[^ ]*')")
 liteloaderqqnt_url="https://github.com/LiteLoaderQQNT/LiteLoaderQQNT/releases/latest/download/LiteLoaderQQNT.zip"
 echo "最新 LiteLoaderQQNT 版本：$liteloaderqqnt_version"
 
 workdir="$PWD"
 
-# 创建临时目录
+# 创建并切换至临时目录
 temp_dir=$(mktemp -d)
 echo "临时目录创建成功: $temp_dir"
 cd "$temp_dir" || exit 1
 
 cleanup() {
     echo "清理临时目录: $temp_dir"
+    read -rp "stop: "
     rm -rf "$temp_dir"
 }
-
 trap cleanup EXIT
-
 
 # 修补 resources，创建 *.js 文件，并修改 package.json
 function patch_resources() {
@@ -51,8 +51,8 @@ function patch_resources() {
 # 获取qq appimage 最新链接
 function get_qqnt_appimage_url() {
     [ "$os_arch" != "x86_64" ] && os_arch="arm64"
-    check_url="$(curl -s https://im.qq.com/linuxqq/index.shtml| grep -o 'https://.*linuxQQDownload.js?[^"]*')"
-    appimage_url=$(curl -s "$check_url" | grep -o 'https://[^,]*AppImage' | grep "$os_arch")
+    check_url="$(wget -t3 -T3 -q -O- https://im.qq.com/linuxqq/index.shtml| grep -o 'https://.*linuxQQDownload.js?[^"]*')"
+    appimage_url=$(wget -t3 -T3 -q -O- "$check_url" | grep -o 'https://[^,]*AppImage' | grep "$os_arch")
 
     [ -z "$appimage_url" ] && { echo "获取qq下载链接失败"; exit 1; }
     echo "$appimage_url"
@@ -62,21 +62,55 @@ function download_url() {
     local url=$1
     local output=$2
     echo "开始下载 $output: $url"
-    if wget -q "$url" -O "$output"; then
+    if wget -t3 -T3 -q -O "$output" "$url"; then
         echo "下载成功：$output"
     else
         echo "下载失败：$url" && exit 1
     fi
 }
 
-# 修改 AppRun 文件以定义配置路径
-function patch_appiamge_apprun() {
-    target_dir="$1"
-    apprun_file="$target_dir/AppRun"
-    profile_dir="\$HOME/.config/QQ/LiteLoader"
-    echo "当前target_dir:$target_dir"
+# 计算 squashfs 偏移值
+function calc_appimage_offset() {
+    local appimage_file="$1"
+    local magic_number="68 73 71 73" #hsqs
 
-    # 检查是否已存在LITELOADERQQNT_PROFILE
+    [ ! -f "$appimage_file" ] && { echo "Error: '$appimage_file' not found!"; exit 1; }
+
+    # 查找魔数并计算偏移值
+    lineno=$(od -A n -t x1 -v -w4 "$appimage_file" | awk "/^ $magic_number/ {print NR; exit}")
+    [ -z "$lineno" ] && { echo "Error: 未找到 squashfs"; exit 1; }
+
+    echo "$(( (lineno-1)*4 ))"
+}
+
+# 提取 appimage 内容
+function extract_appimage() {
+    local appimage_file="$1"
+    if ! ./"$appimage_file" --appimage-extract >/dev/null 2>&1; then
+        echo "执行 --appimage-extract 失败，尝试手动提取..."
+        offset=$(calc_appimage_offset "$appimage_file")
+        output="out.squashfs"
+        echo "squashfs偏移值：$offset"
+        rm -rf squashfs-root
+
+        echo "开始写出 squashfs 文件至 $output"
+        tail -c +"$((offset+1))" "$appimage_file" > "$output"
+        echo "写出成功：$output"
+
+        echo "开始提取 squashfs 内容"
+        unsquashfs "$output" >/dev/null
+        [ ! -d "squashfs-root" ] && { echo "文件提取失败: $output"; exit 1; }
+        echo "文件提取成功"
+        rm "$output" && echo "临时文件 $output 已移除"
+    fi
+}
+
+# 修改 AppRun 文件以支持变量 LITELOADERQQNT_PROFILE
+function patch_appiamge_apprun() {
+    apprun_file="$1/AppRun"
+    profile_dir="\$HOME/.config/QQ/LiteLoader"
+
+    # 检查是否已存在 LITELOADERQQNT_PROFILE
     if grep -q "export LITELOADERQQNT_PROFILE=" "$apprun_file"; then
         sed -i "s|export LITELOADERQQNT_PROFILE=.*|export LITELOADERQQNT_PROFILE=\${LITELOADERQQNT_PROFILE:=$profile_dir}|" "$apprun_file"
     else
@@ -102,9 +136,9 @@ function repack_appimage() {
 # 修改 appimage 文件
 function patch_appiamge() {
     local appimage="$1"
-    echo "开始操作 $appimage"
+    echo "正在对 AppImage 文件进行补丁操作: $appimage"
     chmod +x "$appimage"
-    $appimage --appimage-extract >/dev/null
+    extract_appimage "$appimage"
     patch_appiamge_apprun "squashfs-root"
     patch_resources "squashfs-root/resources/app"
 
